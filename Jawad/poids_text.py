4170ac0210833c4d6f32fbf8e2ceb70c8636b8a9
Kyoma47 <mediane.jawad@gmail.com>
2020-06-05 00:10:33 +0000

    fonction occurences dans poids_text.py

--- Jawad/poids_text.py
+++ Jawad/poids_text.py
@@ -1,7 +1,13 @@
+def nettoyer(texte): #tokenization
+    return texte.translate(
+        str.maketrans('’',' ', string.punctuation + '…\t\n' )
+    )
 
-def occurence(texte):
-    mots = text.split()
+def decouper(texte): #texte sans ponctuation
+    return texte.split()
+
+def occurences(liste_mots):
     D = {}
-    for mot in mots:
-        D[mot] = 0 if mot not in mots else D[mot]+1
+    for mot in liste_mots:
+        D[mot.lower()] = 1 if mot.lower() not in D else D[mot.lower()]+1
     return D
