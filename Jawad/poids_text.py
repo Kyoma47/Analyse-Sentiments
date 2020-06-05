55584fe6cd649e242c88b93f6d4d0dac029babfd
Kyoma47 <mediane.jawad@gmail.com>
2020-06-05 15:33:50 +0000

    emotions champs lexical

--- Jawad/poids_text.py
+++ Jawad/poids_text.py
@@ -8,6 +8,9 @@ def nettoyer(texte): #tokenization
 def decouper(texte): #texte sans ponctuation
     return texte.split()
 
+def trier(liste_mots):
+    liste_mots.sort()
+
 def occurences(liste_mots):
     D = {}
     for mot in liste_mots:
@@ -16,6 +19,14 @@ def occurences(liste_mots):
 
 def occurences_fichier(nom_fichier):
     with open(nom_fichier, "r", encoding="utf-8" ) as fichier :
-        dic = occurences( decouper( nettoyer( fichier.read() ) ) )
+        mots = decouper( nettoyer( fichier.read() ) )
+        trier(mots)
+
+
+        with open("alphabetic_"+ nom_fichier.split("/")[-1], 'w+', encoding="utf-8") as file:
+            file.write('\n')
+            for mot in mots: file.write( mot +'\n')
+
+        dic  = occurences( mots )
         for cle in dic :
             if dic[cle]>1 : print(cle, ":", dic[cle])
