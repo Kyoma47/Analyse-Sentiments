0fe079c646cb11d8b94ddd1a8fa306cca8757293
S-siham <sihamriham97@gmail.com>
2020-06-06 20:05:03 +0100

    Ajout fonction de recherche d'informations depuis le site genius.

---
+++ Siham/Scraping_genuis.py
@@ -0,0 +1,15 @@
+# Web scraping, pickle imports
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Tag as tg
+import pickle
+
+
+def scraping_genuis(url):
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, "lxml")
+    div = soup.find(class_="Lyrics__Container-sc-1ynbvzw-2 jgQsqn")
+    if div != None:
+        return [ligne for ligne in div if type(ligne) != tg]
+
+list = scraping_genuis("https://genius.com/Keenv-les-mots-lyrics")
