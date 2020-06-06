419302e3d2a3fc905eddeeabaef9bdd7d2735263
Kyoma47 <mediane.jawad@gmail.com>
2020-06-06 19:46:31 +0000

    commit while

--- Jawad/scraping.py
+++ Jawad/scraping.py
@@ -3,11 +3,15 @@ from bs4 import BeautifulSoup
 from bs4.element import Tag as tg
 import pickle
 
+
+
 def scraping_genius(url):
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, "lxml")
-    div  = soup.find(class_="Lyrics__Container-sc-1ynbvzw-2 jgQsqn")
-    if div!=None: return [ ligne for ligne in div if type(ligne) != tg ]
-    else: print("class!")#div  = soup.find(class_="song_body-lyrics")
+    print("while!")
+    while True:
+        page = requests.get(url).text
+        soup = BeautifulSoup(page, "lxml")
+        div = soup.find(class_="Lyrics__Container-sc-1ynbvzw-2 jgQsqn")
+        if div!=None: return [ligne for ligne in div if type(ligne) != tg]
+        #div  = soup.find(class_="song_body-lyrics")
 
 liste = scraping_genius("https://genius.com/Keenv-les-mots-lyrics")
